b4cfd7b626055c8bcbdda2cafaea3490e66a94d0
behijabenghorbel <61513399+behijabenghorbel@users.noreply.github.com>
2020-12-07 17:33:21 +0100

    Update Utilisateur.php

--- front-end/model/Utilisateur.php
+++ front-end/model/Utilisateur.php
@@ -1,16 +1,20 @@
 <?PHP
 	class Utilisateur{
-		private ?int $id = null;
-		private ?string $nom = null;
-		private ?string $prenom = null;
-		private ?string $email = null;
-		private ?string $login = null;
-		private ?string $password = null;
+		private  $id = null;
+		private  $nom = null;
+		private  $prenom = null;
+		private  $date_naissance = null;
+		private  $telephone = null;
+		private  $email = null;
+		private  $login = null;
+		private  $password = null;
 		
-		function __construct(string $nom, string $prenom, string $email, string $login, string $password){
+		function __construct(string $nom, string $prenom, string $date_naissance, int $telephone , string $email, string $login, string $password){
 			
 			$this->nom=$nom;
 			$this->prenom=$prenom;
+			$this->date_naissance=$date_naissance;
+			$this->telephone=$telephone;
 			$this->email=$email;
 			$this->login=$login;
 			$this->password=$password;
@@ -25,12 +29,19 @@
 		function getPrenom(): string{
 			return $this->prenom;
 		}
-		function getLogin(): string{
-			return $this->login;
+		function getDateNaissance(): string{
+			return $this->date_naissance;
+		}
+		function getTelephone(): int{
+			return $this->telephone;
 		}
 		function getEmail(): string{
 			return $this->email;
 		}
+		function getLogin(): string{
+			return $this->login;
+		}
+		
 		function getPassword(): string{
 			return $this->password;
 		}
@@ -40,6 +51,12 @@
 		}
 		function setPrenom(string $prenom): void{
 			$this->prenom;
+		}
+		function setDateNaissance(date $date_naissance): void{
+			 $this->date_naissance=$date_naissance;
+		}
+		function setTelephone(int $telephone): void{
+			 $this->telephone=$telephone;
 		}
 		function setLogin(string $login): void{
 			$this->login=$login;

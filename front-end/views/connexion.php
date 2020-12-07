4804512f4b950ccbedba57450afae9a1d1fc39a9
behijabenghorbel <61513399+behijabenghorbel@users.noreply.github.com>
2020-12-07 17:01:32 +0100

    Update connexion.php

--- front-end/views/connexion.php
+++ front-end/views/connexion.php
@@ -1,117 +1,169 @@
 <?php
     include_once '../Model/Utilisateur.php';
     include_once '../Controller/UtilisateurC.php';
-
+    
     $error = "";
-
     // create user
     $user = null;
-
     // create an instance of the controller
     $userC = new UtilisateurC();
     if (
         isset($_POST["nom"]) && 
         isset($_POST["prenom"]) &&
+        isset($_POST["date_naissance"]) && 
+        isset($_POST["telephone"]) &&
         isset($_POST["email"]) && 
         isset($_POST["login"]) && 
-        isset($_POST["pass"])
-    ) {
+        isset($_POST["password"])
+    ) { 
         if (
             !empty($_POST["nom"]) && 
             !empty($_POST["prenom"]) && 
+            !empty($_POST["date_naissance"]) && 
+            !empty($_POST["telephone"]) && 
             !empty($_POST["email"]) && 
             !empty($_POST["login"]) && 
-            !empty($_POST["pass"])
+            !empty($_POST["password"])
         ) {
             $user = new Utilisateur(
                 $_POST['nom'],
                 $_POST['prenom'], 
+                $_POST['date_naissance'],
+                $_POST['telephone'],
                 $_POST['email'],
                 $_POST['login'],
-                $_POST['pass']
+                $_POST['password']
             );
-            $userC->ajouterUtilisateur($user);
-            header('Location:afficherUtilisateurs.php');
+            $userC->ajouterPatient($user);
+            header('Location:afficherPatient.php');
         }
         else
-            $error = "Missing information";
-    }
-
-    
+        $error = "Missing information";
+    }  
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>User Display</title>
-</head>
-    <body>
-        <button><a href="afficherUtilisateurs.php">Retour à la liste</a></button>
-        <hr>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Inscription</title>
+        <link href="styles.css" rel="stylesheet"/>
         
-        <div id="error">
-            <?php echo $error; ?>
-        </div>
-        
-        <form action="" method="POST">
-            <table border="1" align="center">
+    </head>
 
-                <tr>
-                    <td rowspan='3' colspan='1'>Fiche Personnelle</td>
-                    <td>
-                        <label for="nom">Nom:
-                        </label>
-                    </td>
-                    <td><input type="text" name="nom" id="nom" maxlength="20"></td>
-                </tr>
-                <tr>
-                    <td>
-                        <label for="prenom">Prénom:
-                        </label>
-                    </td>
-                    <td><input type="text" name="prenom" id="prenom" maxlength="20"></td>
-                </tr>
-                
-                <tr>
-                    <td>
-                        <label for="email">Adresse mail:
-                        </label>
-                    </td>
-                    <td>
-                        <input type="email" name="email" id="email" pattern=".+@gmail.com|.+@esprit.tn">
-                    </td>
-                </tr>
-                <tr>
-                    <td rowspan='2' colspan='1'>Information de Connexion</td>
-                    <td>
-                        <label for="login">Login:
-                        </label>
-                    </td>
-                    <td>
-                        <input type="text" name="login" id="login" >
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <label for="pass">Mot de passe:
-                        </label>
-                    </td>
-                    <td>
-                        <input type="password" name="pass" id="pass">
-                    </td>
-                </tr>
-                
-                <tr>
-                    <td></td>
-                    <td>
-                        <input type="submit" value="Envoyer"> 
-                    </td>
-                    <td>
-                        <input type="reset" value="Annuler" >
-                    </td>
-                </tr>
-            </table>
-        </form>
+    <body class="bg-primary">
+    <script src="script.js"></script>
+        <div ><a class="btn btn-primary " href="afficherPatient.php">Retour à la liste</a></div>
+        <hr>
+        <div id="error">
+            <?//php echo $error; ?>
+        </div>
+        <div id="layoutAuthentication">
+            <div id="layoutAuthentication_content">
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-lg-7">
+                            <div class="card shadow-lg border-0 rounded-lg mt-5">
+                                <div class="card-header">
+                                    <h1 class="text-center font-weight-light my-4">créer un compte</h1>
+                                </div>
+                                <div class="card-body">
+                                    <form action="" method="POST">
+                                        <table  align="center">
+                                            <tr>
+                                                <td>
+                                                    <label class="small mb-1" for="nom">Nom:</label>
+                                                </td>
+                                                <td>
+                                                    <input class="form-control" type="text" name="nom" id="nom" maxlength="20" placeholder="Entrer le nom">
+                                                </td>
+                                            </tr>
+
+                                            <tr>
+                                                <td>
+                                                    <label class="small mb-1" for="prenom">Prénom:</label>
+                                                </td>
+                                                <td>
+                                                    <input class="form-control" type="text" name="prenom" id="prenom" maxlength="20" placeholder="Entrer le prenom">
+                                                </td>
+                                            </tr>
+
+                                            <tr>
+                                                <td>
+                                                    <label class="small mb-1" for="date_naissance">date de naissance:</label>
+                                                </td>
+                                                <td>
+                                                    <input  class="form-control" type="text" name="date_naissance" id="date_naissance" maxlength="20" >
+                                                </td>
+                                            </tr>
+
+                                            <tr>
+                                                <td>
+                                                    <label class="small mb-1" for="telephone">telephone:</label>
+                                                </td>
+                                                <td>
+                                                    <input class="form-control" type="text" name="telephone" id="telephone" maxlength="20" placeholder="Entrer le numero de telephone">
+                                                </td>
+                                            </tr>
+                                            
+                                            <tr>
+                                                <td>
+                                                    <label class="small mb-1" for="email">Adresse mail:</label>
+                                                </td>
+                                                <td>
+                                                    <input class="form-control" type="email" name="email" id="email" pattern=".+@gmail.com|.+@esprit.tn|.+@yahoo.com|.+@yahoo.fr" placeholder="Entrer l'adresse mail">
+                                                </td>
+                                            </tr>
+
+                                            <tr>
+                                                <td>
+                                                    <label class="small mb-1" for="login">Login:</label>
+                                                </td>
+                                                <td>
+                                                    <input class="form-control" type="text" name="login" id="login" placeholder="Entrer le nom d'utilisateur">
+                                                </td>
+                                            </tr>
+
+                                            <tr>
+                                                <td>
+                                                    <label class="small mb-1" for="password">Mot de passe:</label>
+                                                </td>
+                                                <td>
+                                                    <input class="form-control" type="password" name="password" id="password" placeholder="Entrer le mot de passe">
+                                                </td>
+                                            </tr>
+
+                                            <tr>
+                                                <td>
+                                                    <label class="small mb-1" for="confpassword">Verifier Mot de passe:</label>
+                                                </td>
+                                                <td>
+                                                    <input class="form-control" type="password" name="confpassword" id="confpassword" placeholder="Confirmer le mot de passe">
+                                                </td>
+                                            </tr>
+
+                                </div>
+                                            <tr>
+                                                <td></td>
+                                                <td>
+                                                    <input class="btn btn-primary btn-block" type="submit" value="Envoyer" onclick="verif();"> 
+                                                </td>
+                                            </tr>
+                                            
+                                            <tr>
+                                                <td></td>
+                                                <td>
+                                                    <input class="btn btn-primary btn-block" type="reset" value="Annuler" >
+                                                </td>
+                                            </tr>
+                                        </table>
+                                    </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>                         
+            </div>
+        </div>
     </body>
 </html>

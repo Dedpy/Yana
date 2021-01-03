f5523ae22faf1694d204b2b9e6531def6d6fb9ce
behijabenghorbel <61513399+behijabenghorbel@users.noreply.github.com>
2021-01-03 02:04:00 +0100

    Update motdepasse.php

--- front-end/views/motdepasse.php
+++ front-end/views/motdepasse.php
@@ -1,133 +1,150 @@
 <?php
-include_once '../Model/patient.php';
-require('../config.php');
-session_start();
+    include "../controller/patientC.php";
+    include_once '../Model/patient.php';
 
-if (isset($_POST['email'])){
+	$patientC = new patientC();
+	$error = "";
 
-    $email=$_POST['email'];
-
-    $sql="SELECT * FROM patient WHERE email='" . $email . "'";
-			$db = config::getConnexion();
-			try{
-				$query=$db->prepare($sql);
-				$query->execute();
-                $count=$query->rowCount();
-                if($count==1){
-				$user=$query->fetch();
-               // var_dump($user);
-                $_SESSION['email'] = $email;
-                $_SESSION['login'] = $user['login'];
-                $login=$user['login'];
-                //$_SESSION['id'] = $user['id'];
-                $code=mt_rand(1000,9999);
-                    $sql="UPDATE patient SET code= '" . $code . "' WHERE email='" . $email . "'";
-                    $db = config::getConnexion();
-			    
-				$query1=$db->prepare($sql);
-				$query1->execute();
-
-                
-                     $email1="behijabenghorbel@gmail.com";    
-                     $dest = $email;
-                     $sujet = "Réinitialisation du mot de passe";
-                   $corp =" Bonjour $login voici votre code de verification $code " ;
-                    $headers = 'From: ' .$email1 . "\r\n".'Reply-To: ' . $email1. "\r\n".'X-Mailer: PHP/' . phpversion();
-                     if (mail($dest, $sujet, $corp, $headers)) {
-                        echo "Email envoyé avec succès à $dest ...";
-                    } else {
-                         echo "Échec de l'envoi de l'email...";
-                         }
-                     header("Location: verifpassword.php");
-                        }
-			}
-			catch (Exception $e){
-				die('Erreur: '.$e->getMessage());
-            }
-        } 
-
-	// $email = stripslashes($_REQUEST['email']);
-	// $email = mysqli_real_escape_string($conn, $email);
-	
-    // $query="SELECT password FROM patient WHERE email='" . $email . "'";
-	// $result = mysqli_query($conn,$query) or die(mysql_error());
-    // $rows = mysqli_num_rows($result);
-    
-    // //$query1="UPDATE patient SET code= '" . $code . "'";
-	// if($rows==1){
-
-    //     $_SESSION['email'] = $email;
-    //     $code=mt_rand(1000,9999);
-    //     $query1="UPDATE patient SET code= '" . $code . "' WHERE email='" . $email . "'";
-    //     $result1 = mysqli_query($conn,$query1) or die(mysql_error());
-        
-    //     $email1="behijabenghorbel@gmail.com";    
-    //     $dest = $email;
-    //     $sujet = "Réinitialisation du mot de passe";
-    //     $corp =" Bonjour $email voici votre code de verification $code " ;
-    //     $headers = 'From: ' .$email1 . "\r\n".'Reply-To: ' . $email1. "\r\n".'X-Mailer: PHP/' . phpversion();
-    //     if (mail($dest, $sujet, $corp, $headers)) {
-    //         echo "Email envoyé avec succès à $dest ...";
-    //     } else {
-    //         echo "Échec de l'envoi de l'email...";
-    //         }
-	//     header("Location: verifpassword.php");
-	// }else{
-	// 	echo $message = "L'email est incorrect.";
-	// }
-
+	if (
+        isset($_POST["nom"]) && 
+        isset($_POST["prenom"]) &&
+        isset($_POST["date_naissance"]) &&
+        isset($_POST["telephone"]) && 
+        isset($_POST["email"]) && 
+        isset($_POST["login"]) && 
+        isset($_POST["password"])
+    ){
+		if (
+            !empty($_POST["nom"]) && 
+            !empty($_POST["prenom"]) &&
+            !empty($_POST["date_naissance"]) && 
+            !empty($_POST["telephone"]) &&  
+            !empty($_POST["email"]) && 
+            !empty($_POST["login"]) && 
+            !empty($_POST["password"])
+        ) {
+            if( preg_match ( " /^[a-zA-Z]{2,}$/ " , $_POST['nom'] )==1 && preg_match (" /^[a-zA-Z]{2,}$/ ", $_POST['prenom'] )==1 && preg_match ( ' /^.+@.+\.[a-z]{2,}$/ ' , $_POST['email'] )==1 && preg_match ( ' #^[0-9]{8}+$# ', $_POST['telephone'])==1 ){
+            $user = new patient(
+                $_POST['nom'],
+                $_POST['prenom'], 
+                $_POST['date_naissance'],
+                $_POST['telephone'], 
+                $_POST['email'],
+                $_POST['login'],
+                $_POST['password']
+            );
+            
+            $patientC->modifierPatient($user, $_GET['id']);
+           // header('Location:afficherPatient.php');
+        }else {
+            echo "<br>";echo "<br>";echo "<br>";echo "<br>";echo "<br>";echo "<br>";
+            if($count!=0){echo 'L email existe deja '; echo "<br>";}
+            if(preg_match ( " /^[a-zA-Z]{2,}$/ " , $_POST['nom'] )==0){echo 'Le nom doit contenir que des lettres '; echo "<br>";}
+            if(preg_match ( " /^[a-zA-Z]{2,}$/ " , $_POST['prenom'] )==0){echo 'Le prenom doit contenir que des lettres '; echo "<br>";}
+            if(preg_match ( ' /^.+@.+\.[a-zA-Z]{2,}$/ '  , $_POST['email'] )==0){echo 'L email est incorrect '; echo "<br>";} 
+            if(preg_match ( " /^[0-9]{8}$/ " , $_POST['telephone'] )==0){echo 'Le numero de telephone doit contenir 8 chiffres '; echo "<br>";}
+            
+        }
+    }
+        else
+            $error = "Missing information";
+	}
 
 ?>
-
 <!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>mot de passe oublié</title>
-        <link href="styles.css" rel="stylesheet"/>
-        
-    </head>
-
-    <body class="bg-primary">
-    
-        
-        <div id="error">
-            <?//php echo $error; ?>
-        </div>
-        <div id="layoutAuthentication">
+<html>
+	<head>
+		<title>Modifier mon compte</title>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	</head>
+	<body >
+		<?php
+			if (isset($_GET['id'])){
+				$user = $patientC->recupererPatient1($_GET['id']);	
+		?>
+		<div id="layoutAuthentication">
             <div id="layoutAuthentication_content">
                 <div class="container">
                     <div class="row justify-content-center">
                         <div class="col-lg-7">
                             <div class="card shadow-lg border-0 rounded-lg mt-5">
                                 <div class="card-header">
-                                    <h1 class="text-center font-weight-light my-4">Mot de passe oublié</h1>
+                                    <h1 class="text-center font-weight-light my-4">S'inscrire</h1>
                                 </div>
                                 <div class="card-body">
                                     <form action="" method="POST">
                                         <table  align="center">
-                                           
+                                            <tr>
+                                                <td>
+                                                    <label class="small mb-1" for="nom">Nom:</label>
+                                                </td>
+                                                <td>
+                                                    <input class="form-control" type="text" name="nom" id="nom" maxlength="20" value = "<?php echo $user->nom; ?>">
+                                                </td>
+                                            </tr>
+
+                                            <tr>
+                                                <td>
+                                                    <label class="small mb-1" for="prenom">Prénom:</label>
+                                                </td>
+                                                <td>
+                                                    <input class="form-control" type="text" name="prenom" id="prenom" maxlength="20" value = "<?php echo $user->prenom; ?>">
+                                                </td>
+                                            </tr>
+
+                                            <tr>
+                                                <td>
+                                                    <label class="small mb-1" for="date_naissance">date de naissance:</label>
+                                                </td>
+                                                <td>
+                                                    <input  class="form-control" type="date" name="date_naissance" id="date_naissance" value = "<?php echo $user->date_naissance; ?>">
+                                                </td>
+                                            </tr>
+
+                                            <tr>
+                                                <td>
+                                                    <label class="small mb-1" for="telephone">telephone:</label>
+                                                </td>
+                                                <td>
+                                                    <input class="form-control" type="text" name="telephone" id="telephone" length="8" value = "<?php echo $user->telephone; ?>">
+                                                </td>
+                                            </tr>
                                             
                                             <tr>
                                                 <td>
                                                     <label class="small mb-1" for="email">Adresse mail:</label>
                                                 </td>
                                                 <td>
-                                                    <input class="form-control" type="email" name="email" id="email" pattern=".+@gmail.com|.+@esprit.tn|.+@yahoo.com|.+@yahoo.fr" placeholder="Entrer l'adresse mail">
+                                                    <input class="form-control" type="email" name="email" id="email" pattern=".+@gmail.com|.+@esprit.tn|.+@yahoo.fr|.+yahoo.com" value = "<?php echo $user->email; ?>">
                                                 </td>
                                             </tr>
 
-                                            
+                                            <tr>
+                                                <td>
+                                                    <label class="small mb-1" for="login">Login:</label>
+                                                </td>
+                                                <td>
+                                                    <input class="form-control" type="text" name="login" id="login" value = "<?php echo $user->login; ?>">
+                                                </td>
+                                            </tr>
+
+                                            <tr>
+                                                <td>
+                                                    <label class="small mb-1" for="password">Mot de passe:</label>
+                                                </td>
+                                                <td>
+                                                    <input class="form-control" type="password" name="password" id="password" value = "<?php echo $user->password; ?>">
+                                                </td>
+                                            </tr>
+
+                                    
 
                                 </div>
                                             <tr>
                                                 <td></td>
                                                 <td>
-                                                
-                                                    <input class="btn btn-primary btn-block" type="submit" value="Envoyer" > 
-                                                   
-
+                                                    <input class="btn btn-primary btn-block" type="submit" value="Envoyer" onclick="verif();"> 
                                                 </td>
                                             </tr>
                                             
@@ -145,5 +162,9 @@ if (isset($_POST['email'])){
                 </div>                         
             </div>
         </div>
-    </body>
+
+		<?php
+		}
+		?>
+	</body>
 </html>
